4119b61974f711167ffe0b07653f3cb191b4c3be
jong-k <fully.charged07@gmail.com>
2025-06-23 13:08:43 +0900

    feat: 스택 내용 추가

---
+++ 10-스택과큐/stack.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (this.size === 0) return null;
+    const removedNode = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      removedNode.next = null;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+}
