ec21bc2b2c054d575ceb1fec9c0dc7b78dadc92b
jong-k <fully.charged07@gmail.com>
2025-06-26 15:49:03 +0900

    feat: 그래프 순회 내용 추가

--- 14-그래프/graph.js
+++ 14-그래프/graph.js
@@ -34,4 +34,68 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  /*
+  인접리스트 예시
+  {
+    A: [B, C],
+    B: [A, D],
+    C: [A, E],
+    D: [B, E, F],
+    E: [C, D, F],
+    F: [D, E]
+  }
+  */
+  recursiveDFS(start) {
+    const visited = {};
+    const result = [];
+
+    const dfs = (vertex) => {
+      if (!this.adjacencyList[vertex]) return;
+      visited[vertex] = true;
+      result.push(vertex);
+
+      this.adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) dfs(neighbor);
+      });
+    };
+    dfs(start);
+    return result;
+  }
+
+  iterativeDFS(start) {
+    const stack = [start];
+    const visited = { [start]: true };
+    const result = [];
+
+    while (stack.length) {
+      const currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  BFS(start) {
+    const queue = [start];
+    const visited = { [start]: true };
+    const result = [];
+
+    while (queue.length) {
+      const currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+  }
 }
