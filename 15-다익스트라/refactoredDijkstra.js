d438a08ef78638369df6b5646ebdb2d0b7aee59d
jong-k <fully.charged07@gmail.com>
2025-06-27 15:16:19 +0900

    refactor: 다익스트라 이진힙 리팩토링

--- 15-다익스트라/refactoredDijkstra.js
+++ 15-다익스트라/refactoredDijkstra.js
@@ -1,15 +1,80 @@
+// 이진 힙 우선순위 큐를 사용하여 시간 복잡도 개선
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
 class PriorityQueue {
   constructor() {
     this.queue = [];
   }
 
   enqueue(value, priority) {
-    this.queue.push({ value, priority });
-    this.queue.sort((a, b) => a.priority - b.priority);
+    const newNode = new Node(value, priority);
+    this.queue.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.queue.length - 1;
+    const node = this.queue[index];
+
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parentNode = this.queue[parentIndex];
+
+      if (node.priority >= parentNode.priority) break;
+
+      this.queue[index] = parentNode;
+      this.queue[parentIndex] = node;
+      index = parentIndex;
+    }
   }
 
   dequeue() {
-    return this.queue.shift();
+    if (this.queue.length === 0) return undefined;
+
+    const minNode = this.queue[0];
+    const lastNode = this.queue.pop();
+
+    if (this.queue.length > 0) {
+      this.queue[0] = lastNode;
+      this.sinkDown();
+    }
+    return minNode;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.queue.length;
+    const node = this.queue[0];
+
+    while (true) {
+      const leftIndex = 2 * index + 1;
+      const rightIndex = 2 * index + 2;
+      let leftNode, rightNode;
+      let swapIndex = null;
+
+      if (leftIndex < length) {
+        leftNode = this.queue[leftIndex];
+        if (leftNode.priority < node.priority) swapIndex = leftIndex;
+      }
+      if (rightIndex < length) {
+        rightNode = this.queue[rightIndex];
+        if (
+          (!swapIndex && rightNode.priority < node.priority) ||
+          (swapIndex && rightNode.priority < leftNode.priority)
+        )
+          swapIndex = rightIndex;
+      }
+      if (!swapIndex) break;
+      // swap
+      this.queue[index] = this.queue[swapIndex];
+      this.queue[swapIndex] = node;
+      index = swapIndex;
+    }
   }
 }
 
@@ -31,16 +96,6 @@ class WeightedGraph {
       this.adjacencyList[vertex2].push({ vertex: vertex1, weight });
   }
 
-  /*
-인접 리스트 예시
-A: [{ vertex: 'B', weight: 4 }, { vertex: 'C', weight: 2 }],
-B: [{ vertex: 'A', weight: 4 }, { vertex: 'E', weight: 3}],
-C: [{ vertex: 'A', weight: 2 }, { vertex: 'F', weight: 4}],
-D: [{ vertex: 'C', weight: 2 }, { vertex: 'E', weight: 3}, { vertex: 'F', weight: 1}],
-E: [{ vertex: 'B', weight: 3 }, { vertex: 'D', weight: 3}, { vertex: 'F', weight: 1}],
-F: [{ vertex: 'C', weight: 4 }, { vertex: 'D', weight: 1}, { vertex: 'E', weight: 1}],
-*/
-
   dijkstra(start, end) {
     const distances = {};
     const previous = {};
@@ -48,7 +103,6 @@ F: [{ vertex: 'C', weight: 4 }, { vertex: 'D', weight: 1}, { vertex: 'E', weight
     const path = [];
     let smallestNode;
 
-    // 초기값 입력
     for (const vertex in this.adjacencyList) {
       if (vertex === start) {
         distances[vertex] = 0;
@@ -74,7 +128,6 @@ F: [{ vertex: 'C', weight: 4 }, { vertex: 'D', weight: 1}, { vertex: 'E', weight
 
       if (smallestNode || distances[smallestNode] !== Number.MAX_SAFE_INTEGER) {
         for (const neighbor of this.adjacencyList[smallestNode]) {
-          // neighbor: { vertex: 'B', weight: 4 }
           const candidateDistance = distances[smallestNode] + neighbor.weight;
           const neighborVertex = neighbor.vertex;
           if (candidateDistance < distances[neighborVertex]) {
@@ -91,23 +144,3 @@ F: [{ vertex: 'C', weight: 4 }, { vertex: 'D', weight: 1}, { vertex: 'E', weight
     };
   }
 }
-
-const g = new WeightedGraph();
-
-g.addVertex("A");
-g.addVertex("B");
-g.addVertex("C");
-g.addVertex("D");
-g.addVertex("E");
-g.addVertex("F");
-
-g.addEdge("A", "B", 4);
-g.addEdge("A", "C", 2);
-g.addEdge("B", "E", 3);
-g.addEdge("C", "D", 2);
-g.addEdge("C", "F", 4);
-g.addEdge("D", "E", 3);
-g.addEdge("D", "F", 1);
-g.addEdge("F", "E", 1);
-
-console.log(g.dijkstra("A", "E"));
