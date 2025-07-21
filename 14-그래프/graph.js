25bad20dcf48d3c6a8f1a562b04d2c6e882a63c6
jong-k <fully.charged07@gmail.com>
2025-07-21 11:24:37 +0900

    feat: 그래프 순회 테스트 케이스 추가

--- 14-그래프/graph.js
+++ 14-그래프/graph.js
@@ -97,5 +97,68 @@ class Graph {
         }
       });
     }
+    return result;
   }
 }
+
+const graph = new Graph();
+
+graph.addVertex("S");
+graph.addVertex("P");
+graph.addVertex("U");
+graph.addVertex("X");
+graph.addVertex("Q");
+graph.addVertex("Y");
+graph.addVertex("V");
+graph.addVertex("R");
+graph.addVertex("W");
+graph.addVertex("T");
+
+graph.addEdge("S", "P");
+graph.addEdge("S", "U");
+
+graph.addEdge("P", "X");
+graph.addEdge("U", "X");
+
+graph.addEdge("P", "Q");
+graph.addEdge("U", "V");
+
+graph.addEdge("X", "Q");
+graph.addEdge("X", "Y");
+graph.addEdge("X", "V");
+
+graph.addEdge("Q", "R");
+graph.addEdge("Y", "R");
+
+graph.addEdge("Y", "W");
+graph.addEdge("V", "W");
+
+graph.addEdge("R", "T");
+graph.addEdge("W", "T");
+
+console.log(graph.recursiveDFS("S"));
+/*
+[
+  'S', 'P', 'X', 'U',
+  'V', 'W', 'Y', 'R',
+  'Q', 'T'
+]
+*/
+
+console.log(graph.iterativeDFS("S"));
+/*
+[
+  'S', 'U', 'V', 'W',
+  'T', 'R', 'Q', 'Y',
+  'X', 'P'
+]
+*/
+
+console.log(graph.BFS("S"));
+/*
+[
+  'S', 'P', 'U', 'X',
+  'Q', 'V', 'Y', 'R',
+  'W', 'T'
+]
+*/
