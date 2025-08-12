67dda8e440ed03e76efa23c091d91ecc009484aa
jong-k <fully.charged07@gmail.com>
2025-08-12 11:42:33 +0900

    feat: 가중치 그래프 예제 추가

--- 15-다익스트라/weightedGraph.js
+++ 15-다익스트라/weightedGraph.js
@@ -16,3 +16,17 @@ class WeightedGraph {
       this.adjacencyList[vertex2].push({ vertex: vertex1, weight });
   }
 }
+
+const graph = new WeightedGraph();
+graph.addVertex(1);
+graph.addVertex(2);
+graph.addVertex(3);
+graph.addVertex(4);
+graph.addVertex(5);
+
+graph.addEdge(1, 3, 2);
+graph.addEdge(3, 4, 3);
+graph.addEdge(2, 4, 4);
+graph.addEdge(4, 5, 6);
+
+console.log(graph.adjacencyList);
